634da1146eece3012650eeb4a2bf2a0e8324dcd4
JamesAX <soarxa@gmail.com>
2015-08-31 17:22:06 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,21 @@
+jQuery.sap.declare("huawei.cmes.quality.setting.Config");
+
+
+huawei.cmes.quality.setting.Config = {
+/* 
+  getTileIcons : function(){
+    var icons = ["sap-icon://product","sap-icon://lateness","sap-icon://person-placeholder","sap-icon://alert"];
+    return icons;
+  }*/
+
+    
+ 
+};
+
+huawei.cmes.quality.setting.Config.tileIcons = ["sap-icon://product","sap-icon://lateness","sap-icon://person-placeholder","sap-icon://alert"];
+
+huawei.cmes.quality.setting.Config.periodSpecKPIs = ["MMIN01","MMIN02","CCN","CCR","DFR"];
+
+huawei.cmes.quality.setting.Config.defectKPIs = ["TPY","DDR","DPMO","LAR","RIDPPM","FDPPM","MMQI","DFR","CCI","MMQI","MPQI"];
+
+huawei.cmes.quality.setting.Config.percentValueYAxis = ["LAR","LAR_MAT_CEG","LAR_MAT_CATE_B","LAR_AT_CATE_S"];
