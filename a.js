b067727bf67b3f959ae53a7e6fbeffe78f308f17
JamesAX <soarxa@gmail.com>
2015-09-08 09:15:39 +0800

    Update a.js

--- a.js
+++ a.js
@@ -12,10 +12,14 @@ huawei.cmes.quality.setting.Config = {
  
 };
 
+//The tile icons on column 1 for 4 sub-components
 huawei.cmes.quality.setting.Config.tileIcons = ["sap-icon://product","sap-icon://lateness","sap-icon://person-placeholder","sap-icon://alert"];
 
+//The KPIs that only Year and Month are the valid period
 huawei.cmes.quality.setting.Config.periodSpecKPIs = ["MMIN01","MMIN02","CCN","CCR","DFR"];
 
+//Only these KPIs that need to show defect chart area 
 huawei.cmes.quality.setting.Config.defectKPIs = ["TPY","DDR","DPMO","LAR","RIDPPM","FDPPM","MMQI","DFR","CCI","MMQI","MPQI"];
 
+//The KPIs that need to adjust the Yaxis values
 huawei.cmes.quality.setting.Config.percentValueYAxis = ["LAR","LAR_MAT_CEG","LAR_MAT_CATE_B","LAR_AT_CATE_S"];
